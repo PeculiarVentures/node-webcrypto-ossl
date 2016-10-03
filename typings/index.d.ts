07231f5b5cd0017bf0f7065d464f786ee5dce2cf
microshine <microshine@mail.ru>
2016-10-04 01:05:12 +0300

    Add webcrypto-core typings

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/mkdirp/index.d.ts" />
+/// <reference path="modules/webcrypto-core/index.d.ts" />
