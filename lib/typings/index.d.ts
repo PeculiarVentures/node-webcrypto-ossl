604d1cca63fb9f306441c2f030b334decbba84e6
microshine <microshine@mail.ru>
2016-10-12 06:34:39 +0300

    Add @types/mkdirp

--- lib/typings/index.d.ts
+++ lib/typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="../../node_modules/webcrypto-core/build/webcrypto-core.d.ts" />
+/// <reference path="../../node_modules/mkdirp/index.d.ts" />
 
 type NodeBufferSource = BufferSource | Buffer;
