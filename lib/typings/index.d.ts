f926a7b4f949f0b4e48a539174aa4f9cce2e0c83
microshine <microshine@mail.ru>
2016-10-12 06:52:30 +0300

    update TS linking

--- lib/typings/index.d.ts
+++ lib/typings/index.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="../../node_modules/webcrypto-core/build/webcrypto-core.d.ts" />
-/// <reference path="../../node_modules/mkdirp/index.d.ts" />
+/// <reference path="../../node_modules/@types/mkdirp/index.d.ts" />
 
 type NodeBufferSource = BufferSource | Buffer;
